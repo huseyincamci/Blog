e77749987da12311c4d7a740c879c45b3c603a6d
huseyincamci <hcamci.ce@gmail.com>
2017-05-02 18:55:09 +0300

    Anasayfada makaleler listelendi.

--- Blog.Web.UI/Controllers/HomeController.cs
+++ Blog.Web.UI/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
-﻿using System.Web.Mvc;
+﻿using Blog.BLL.DataServices;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Blog.Web.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private MakaleRepository _makaleRepository;
+
+        public HomeController()
+        {
+            _makaleRepository = new MakaleRepository();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var makaleler = _makaleRepository.GetList().OrderByDescending(x => x.CreatedDate);
+            return View(makaleler);
         }
     }
 }
