4c6b25740592cfd4025f4deb76120299892b3e23
huseyincamci <hcamci.ce@gmail.com>
2017-05-08 21:10:19 +0300

    Anasayfada blog postları için sayfalama yapıldı.

--- Blog.Web.UI/Controllers/HomeController.cs
+++ Blog.Web.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.BLL.DataServices;
+using PagedList;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -6,16 +7,16 @@ namespace Blog.Web.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private MakaleRepository _makaleRepository;
+        private readonly MakaleRepository _makaleRepository;
 
         public HomeController()
         {
             _makaleRepository = new MakaleRepository();
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int sayfa = 1)
         {
-            var makaleler = _makaleRepository.GetList().OrderByDescending(x => x.CreatedDate);
+            var makaleler = _makaleRepository.GetList().OrderByDescending(x => x.CreatedDate).ToPagedList(sayfa, 4);
             return View(makaleler);
         }
     }
